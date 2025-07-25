19f7a7e4a4028303ddf1e41d836aa506719a532a
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-07-25 13:00:46 +0530

    stored procedure for login

--- spRegister.sql
+++ spRegister.sql
@@ -7,9 +7,13 @@ CREATE PROCEDURE RegisterUser(
  IN p_address VARCHAR(255)
 )
 BEGIN
-    INSERT INTO users (username, password, email, address)
-    VALUES (p_username, p_password, p_email, p_address);
+ INSERT INTO users (username, password, email, address)
+ VALUES (p_username, p_password, p_email, p_address);
 END //
 
 DELIMITER ;
+
+
+
+
 
