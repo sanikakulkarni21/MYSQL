2d2be759fcfb14adbc9ec2c51a106746e9cf6217
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-09-10 23:23:32 +0530

    queries using inner join

--- tflassessmentpractise/querydb.sql
+++ tflassessmentpractise/querydb.sql
@@ -3,3 +3,10 @@ select * from questionbank where subjectid=(select id from subjects where title 
 
 select * from questionbank inner join testquestions on testquestions.questionbankid = questionbank.id where testquestions.testid=1;
 
+select * from questionbank inner join testquestions on testquestions.questionbankid = questionbank.id where testquestions.testid=1;
+
+SELECT s.title AS subject_title, qb.id AS question_bank_id, qb.title AS question_title, ec.title AS criteria_title
+FROM questionbank qb
+INNER JOIN evaluationcriterias ec ON qb.evaluationcriteriaid = ec.id
+INNER JOIN subjects s ON qb.subjectid = s.id;
+
