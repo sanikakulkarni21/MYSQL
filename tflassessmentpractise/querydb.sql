48c6e8508c1bba4cbcd9aa3fd7c6903bfec40f8f
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2025-09-10 23:25:58 +0530

    queries usinginner join

--- tflassessmentpractise/querydb.sql
+++ tflassessmentpractise/querydb.sql
@@ -10,3 +10,12 @@ FROM questionbank qb
 INNER JOIN evaluationcriterias ec ON qb.evaluationcriteriaid = ec.id
 INNER JOIN subjects s ON qb.subjectid = s.id;
 
+select questionbank.id, questionbank.title, subjects.title as subject ,evaluationcriterias.title as criteria
+from questionbank, subjects,evaluationcriterias
+where questionbank.subjectid=subjects.id and questionbank.evaluationcriteriaid=evaluationcriterias.id
+and subjects.id=1 and evaluationcriterias.id=1;
+                            
+select employees.firstname,employees.lastname,interviews.candidateid from employees
+inner join interviews
+where employees.id=interviews.candidateid
+order by interviews.candidateid asc;
